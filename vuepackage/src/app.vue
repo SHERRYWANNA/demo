f6be7dc4e4dca8b9d91ffbde2cc8ad56a0bdafb2
SHERRYWANNA <243654677@163.com>
2018-08-13 19:48:13 +0800

    add new vue package

---
+++ vuepackage/src/app.vue
@@ -0,0 +1,17 @@
+<template>
+	<p>my name is {{name}}</p>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data() => {
+			name: 'sherry'
+		}
+	};
+</script>
+
+<style scoped>
+	p {
+		color: #cf3f25;
+	}
+</style>
